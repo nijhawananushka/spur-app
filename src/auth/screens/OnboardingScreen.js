080f4a2ccaea6d1c4b16dfcdb661b8ed5a81b458
Anushka Nijhawan <55636931+nijhawananushka@users.noreply.github.com>
2023-05-14 14:44:29 -0400

    added instruction comments for onboarding screen

--- src/auth/screens/OnboardingScreen.js
+++ src/auth/screens/OnboardingScreen.js
@@ -1,6 +1,14 @@
 import { View, Text } from 'react-native';
 import LogOutButton from '../../feed/components/logOutButton';
 
+
+// Create components for adding Username
+    // query database to ensure username is unique
+    // add user and related info to 'UserProfile' in firestore
+// Set Async state to hold userToken!!! VERY IMPORTANT
+// Navigate to Main Feed if successful
+// Navigate back to sign up screen if unsuccessful
+
 const OnboardingScreen = ({ navigation }) => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
