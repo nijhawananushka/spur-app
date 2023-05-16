4f94ffd7f8b16c393b89822c20cb18f23bcc6b9f
Anushka Nijhawan <55636931+nijhawananushka@users.noreply.github.com>
2023-05-15 22:55:36 -0400

    Added ionicons

--- src/addEvent/screen/AddEventScreen.js
+++ src/addEvent/screen/AddEventScreen.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import addEventStyles from '../styles/AddEventStyles';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-ionicons'
 
 const AddEventScreen = ({ navigation }) => {
     return (
     <View style={addEventStyles.container}>
         <View style={addEventStyles.header}>
-          <Ionicons name="ios-close-outline" size={30} color="#6B6363" onPress={() => navigation.goBack()} />
+          <Icon name="ios-close-outline" size={30} color="#6B6363" onPress={() => navigation.goBack()} />
           <Text style={addEventStyles.headerText}>spur it up</Text>
-          <Ionicons name="ios-checkmark-outline" size={30} color="#6B6363" onPress={() => navigation.goBack()} />
+          <Icon name="ios-checkmark-outline" size={30} color="#6B6363" onPress={() => navigation.goBack()} />
         </View>
         <View style={addEventStyles.roundedContainer}>
         </View>
