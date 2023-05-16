678b7f0ab4740c58a449c68e81803d08a85b2bec
Anushka Nijhawan <55636931+nijhawananushka@users.noreply.github.com>
2023-05-16 16:50:38 -0400

    adding camera support to addevent

--- src/addEvent/screen/AddEventScreen.js
+++ src/addEvent/screen/AddEventScreen.js
@@ -4,43 +4,28 @@ import addEventStyles from '../styles/AddEventStyles';
 import ImagePicker from 'react-native-image-picker';
 import { StyleSheet } from 'react-native';
 import {TitleInputComponent, DescriptionInputComponent} from '../components/InputComponents';
+import Camera from '../components/Camera';
+
 const AddEventScreen = ({ navigation }) => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-  const [imageURI, setImageURI] = useState(null);
   
-  const openCamera = () => {
-    const options = {
-      mediaType: 'photo',
-      quality: 1,
-    };
-
-    ImagePicker.launchCamera(options, (response) => {
-      if (response.uri) {
-        setImageURI(response.uri);
-      }
-    });
-  };
-
   return (
     <View style={addEventStyles.container}>
       <View style={addEventStyles.header}>
-        <TouchableOpacity onPress={() => { navigation.goBack(); }}>
+        {/* <TouchableOpacity onPress={() => { navigation.goBack(); }}>
           <Text style={addEventStyles.headerText}> &lt; </Text>
-        </TouchableOpacity>
-        <Text style={addEventStyles.headerText}>spur it up</Text>
+        </TouchableOpacity> */}
+        <Camera />
+        {/* <Text style={addEventStyles.headerText}>spur it up</Text>
         <TouchableOpacity onPress={() => { Alert.alert('Button 2 pressed'); }}>
           <Text style={addEventStyles.headerText}> &gt; </Text>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
       </View>
       <View style={addEventStyles.roundedContainer}>
         <View style={addEventStyles.inputContainer}>
           <TitleInputComponent onTitleChange={setTitle} />
           <DescriptionInputComponent onDescriptionChange={setDescription} />
-          <View style={styles.imageContainer}>
-            {imageURI && <Image source={{ uri: imageURI }} style={styles.image} />}
-          </View>
-          <TouchableOpacity style={styles.cameraButton} onPress={openCamera}></TouchableOpacity>
         </View>
       </View>
     </View>
@@ -48,31 +33,3 @@ const AddEventScreen = ({ navigation }) => {
 };
 
 export default AddEventScreen;
-
-const styles = StyleSheet.create({
-  imageContainer: {
-    marginTop: 20,
-    alignItems: 'center',
-  },
-
-  image: {
-    width: 200,
-    height: 200,
-    resizeMode: 'cover',
-  },
-
-  cameraButton: {
-    marginTop: 20,
-    backgroundColor: 'blue',
-    paddingHorizontal: 16,
-    paddingVertical: 10,
-    borderRadius: 5,
-    alignItems: 'center',
-  },
-
-  cameraButtonText: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-});
--- src/addEvent/styles/AddEventStyles.js
+++ src/addEvent/styles/AddEventStyles.js
@@ -2,24 +2,22 @@ import { StyleSheet } from 'react-native';
 
 const addEventStyles = StyleSheet.create({
     container: {
-        flex: 1,
+        // flex: 1,
         backgroundColor: '#A5A5A5',
     },
     header: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-        marginTop: '20%',
-        marginLeft: '5%',
-        marginRight: '5%',
-        marginBottom: '5%',
+        zIndex: -1,
+        height: '30%',
     },
     headerText: {
         fontSize: 20,
         fontFamily: 'Inter-Regular',
         fontWeight: 300,
         color: '#483F3F',
-        lineHeight: 24.2,
+        lineHeight: '20%',
     },
     roundedContainer: {
         backgroundColor: '#FEFDF9',
@@ -27,9 +25,8 @@ const addEventStyles = StyleSheet.create({
         borderTopLeftRadius: 20,
         height: '85%',
         width: '100%',
-        bottom: 0,
+        bottom: '3%',
         flexDirection: 'column',
-        flex: 1,
         alignContent: 'center',
         alignItems: 'center',
     },
