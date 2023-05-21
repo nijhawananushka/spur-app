21e8a9769c243ac1b83e331b22e3ae362d708988
Sneha <sneha.squasher@gmail.com>
2023-05-21 10:26:01 -0400

    fixed errors

--- src/events/components/otherEventCard.js
+++ src/events/components/otherEventCard.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import styles from '../styles/components/otherEventCardStyles';
 
 const OtherEventCard = ({ event }) => {
-  const formattedDateTime = new Date(event.dateTime.seconds * 1000).toLocaleString('en-US', {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-  });
+  const [randomColor, setRandomColor] = useState('#F6BCD5');
 
-  const pastelColors = ['#F6BCD5', '#BCC5F3', '#BCEBF3', '#E8ECF8'];
-  const randomColor = pastelColors[Math.floor(Math.random() * pastelColors.length)];
+  useEffect(() => {
+    const pastelColors = ['#F6BCD5', '#BCC5F3', '#BCEBF3', '#E8ECF8'];
+    setRandomColor(pastelColors[Math.floor(Math.random() * pastelColors.length)]);
+  }, []);
+
+  let formattedDateTime = '';
+  if (event.dateTime && event.dateTime.seconds) {
+    formattedDateTime = new Date(event.dateTime.seconds * 1000).toLocaleString('en-US', {
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+    });
+  }
 
   return (
     <View style={[styles.card, { borderColor: randomColor }]}>
