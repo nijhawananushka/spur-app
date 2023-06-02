57cfc268a806e858633ef1173344cd18ec71d5ff
Anushka Nijhawan <55636931+nijhawananushka@users.noreply.github.com>
2023-06-02 19:04:30 +0100

    fixed refresh token

--- src/utils/AuthorizationTokens.js
+++ src/utils/AuthorizationTokens.js
@@ -1,19 +1,31 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Alert } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Alert } from 'react-native';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 export const refreshToken = async () => {
-    try {
-        const { accessToken } = await GoogleSignin.signInSilently();
-        if (!accessToken) {
-            console.log('No access token received');
-            return null;
-        }
-        
-        await AsyncStorage.setItem('calAccessToken', accessToken);
-        return accessToken;
-    } catch (error) {
-        console.error('Failed to refresh token:', error);
-        return null;
+  try {
+    const refreshToken = await AsyncStorage.getItem('calRefreshToken');
+    if (!refreshToken) {
+      console.log('No refresh token found in AsyncStorage');
+      return null;
     }
+
+    const { idToken, serverAuthCode } = await GoogleSignin.signInSilently();
+    if (!idToken) {
+      console.log('No ID token received');
+      return null;
+    }
+
+    const { accessToken } = await GoogleSignin.getTokens();
+    if (!accessToken) {
+      console.log('No access token received');
+      return null;
+    }
+
+    await AsyncStorage.setItem('calAccessToken', accessToken);
+    return accessToken;
+  } catch (error) {
+    console.error('Failed to refresh token:', error);
+    return null;
+  }
 };
